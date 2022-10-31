e1b26bb70e471198d73286dd5af1522e08673ea2
adurga-ariqt <adurga@ariqt.com>
2022-10-31 12:44:17 +0530

    Add project files.

---
+++ Exercise21/Equals20True.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise21
+{
+    internal class Equals20True
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            int result;
+
+            Console.WriteLine("\nInput an integer:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input another integer:");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(x == 20 || y == 20 || (x + y == 20));
+        }
+    }
+}
